f9173def2684543af900cc05c5ea21b0bc41e925
Thibault NORMAND <me@zenithar.org>
2007-03-09 14:13:25 +0000

    - Splitting the ControlCenter and the Gui Part. + Add new architecture for message processing

---
+++ src/include/msgs.h
@@ -0,0 +1,21 @@
+//
+// C++ Interface: msgs
+//
+// Description: 
+//
+//
+// Author: Thibault Normand <thibault.normand@gmail.com>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef __MSGS_H__
+#define __MSGS_H__
+
+/*********************************************************************************************
+ * Bus
+ *********************************************************************************************/
+#include "msg/bus/bus_startmsg.h"
+
+#endif
