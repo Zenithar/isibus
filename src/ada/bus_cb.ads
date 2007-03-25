12096e007ab9f8f9bfe77f6794eaa313a821aaae
frederic.becquier <frederic.becquier@gmail.com>
2007-03-25 09:36:01 +0000

    Now the bus is able to receive the bus line the bus fallows the instructions given by the bus_line After being at the last station the bus wait a new line.

--- src/ada/bus_cb.ads
+++ src/ada/bus_cb.ads
@@ -1,8 +1,14 @@
 with Ivy; use Ivy;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Bus_Cb is
 
 procedure position(	app : AppClientPtr_T;
+                	user_data : UserData_T;
+                	argc : C_Int_T;
+                	argv : C_Char_Etoile_Etoile_T);
+
+procedure init(	app : AppClientPtr_T;
                 	user_data : UserData_T;
                 	argc : C_Int_T;
                 	argv : C_Char_Etoile_Etoile_T);
--- src/ada/bus_de_ligne.ads
+++ src/ada/bus_de_ligne.ads
@@ -1,6 +1,25 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package bus_de_ligne is
 
+--Nb max d'arret sur une ligne
+maxStation : constant integer := 7;
+
+--Declaration du type circuit
+type road is record
+	num : integer;
+	length : integer;
+end record;
+
+type circuit is array(1..maxStation) of road;
+
 procedure start;
+
 procedure position(nb : in integer);
+
+procedure init(	bus_id : in integer;
+		nb_passengers : in integer;
+		bus_line_id : in integer;
+		bus_line : in circuit);
 
 end bus_de_ligne;
