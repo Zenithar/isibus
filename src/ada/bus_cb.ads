5fbe1df722b7307e884c72a8bc52c0ea28c7adc6
frederic.becquier <frederic.becquier@gmail.com>
2007-03-26 09:48:03 +0000

    Implementation of the speed control of the bus

--- src/ada/bus_cb.ads
+++ src/ada/bus_cb.ads
@@ -18,4 +18,9 @@ procedure nextStop(	app : AppClientPtr_T;
                 	argc : C_Int_T;
                 	argv : C_Char_Etoile_Etoile_T);
 
+procedure speed(	app : AppClientPtr_T;
+                	user_data : UserData_T;
+                	argc : C_Int_T;
+                	argv : C_Char_Etoile_Etoile_T);
+
 end Bus_Cb;
